d9855026ab48f23dadcdcfe9930e78f9a0b2c9ed
sunayanapandra <pandrasunayana@gmail.com>
2022-05-05 17:23:37 +0530

    added src pom.xml

---
+++ src/main/java/com/groceryapp/GroceryappUserMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.groceryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryappUserMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryappUserMsApiApplication.class, args);
+		System.out.println("ms user working....");
+	}
+
+}
---
+++ src/test/java/com/groceryapp/GroceryappUserMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryappUserMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
